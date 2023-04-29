bfcd4c01bb69bab558e7c2a1eb97c5d01f006225
RanaAbdulrhman <ranaAbdulrhmanD@gmail.com>
2023-04-29 20:37:31 +0300

    Add dots to the dice

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,12 @@
 export default function Die({value, isHeld, flipHeld}){
+
+  const dots = () =>[...Array(value)].map( elem => <span className="dot block w-2 h-2 rounded-full bg-black" />);
+
   return (
-    <button onClick={flipHeld} className={`w-12 p-3 rounded-lg box-shadow font-bold text-xl cursor-pointer ${isHeld? "bg-[#59E391]": ""}`}>
-      {value}
+    <button onClick={flipHeld} className={`${value > 3 ? "flex justify-center items-center ": ""} w-12 h-12 px-3 rounded-lg box-shadow ${isHeld? "bg-[#59E391]": ""}`}>
+      <div className={`dice dice-${value}`}>
+        {dots()}
+      </div>
     </button>
   )
 }
